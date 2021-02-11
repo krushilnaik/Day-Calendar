17a58e99d3b6c7d33663ddbe14c2e89ed69ce52d
Krushil <krushilnaik96@gmail.com>
2021-02-11 10:35:45 -0500

    print the current date in the header

---
+++ assets/js/dist/script.js
@@ -0,0 +1,7 @@
+// import moment from 'moment';
+var currentDay = document.getElementById("currentDay");
+var timetable = document.querySelector(".container");
+var today = moment().format("dddd, MMMM Do");
+var thisHour = moment().hour();
+console.log(thisHour);
+currentDay.innerText = today;
